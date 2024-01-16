cb3e8f2906d1538db2814bb63747eb9c6ccc3d6c
Mahmoud.Eid <39015047+Mado007@users.noreply.github.com>
2024-01-16 17:57:57 +0200

    fix async def measure_runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""Define measure_runtime coroutine."""
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Execute async_comprehension four times in parallel."""
+    import asyncio
+    import time
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    stop = time.perf_counter()
+    return stop - start
