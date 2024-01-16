c360b854292d15077550e39803d3eed66a56e988
Mahmoud.Eid <39015047+Mado007@users.noreply.github.com>
2024-01-16 17:56:12 +0200

    fix async def

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,3 +3,7 @@
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
+
+async def async_comprehension() -> List[float]:
+    """Return 10 random numbers using async_generator."""
+    return [i async for i in async_generator()]
